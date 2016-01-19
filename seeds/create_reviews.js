d9c144492fff4f32509c9a9d0c14c691ea0002d6
marthaberner <marthaberner.mb@gmail.com>
2016-01-18 22:52:54 -0700

    fix seeds file to include actual reviws movie_id

--- seeds/create_reviews.js
+++ seeds/create_reviews.js
@@ -1,10 +1,14 @@
+var config = require('../knexfile')['development'];
+var db = require('knex')(config);
 
 exports.seed = function(knex, Promise) {
-  return Promise.join(
-    knex('reviews').del(),
+  return db('movies').select().then(function (movies) {
+    return Promise.join(
+      knex('reviews').del(),
 
-    knex('reviews').insert({id: 1, movie_id: 1, author: 'David Bowie', rating: 5}),
-    knex('reviews').insert({id: 1, movie_id: 1, author: 'Aanal Patel', rating: 4}),
-    knex('reviews').insert({id: 2, movie_id: 3, author: 'Clive Barker', rating: 5})
-  );
+      knex('reviews').insert({movie_id: movies[0].id, author: 'David Bowie', rating: 5}),
+      knex('reviews').insert({movie_id: movies[0].id, author: 'Aanal Patel', rating: 4}),
+      knex('reviews').insert({movie_id: movies[2].id, author: 'Clive Barker', rating: 5})
+    );
+  })
 };
