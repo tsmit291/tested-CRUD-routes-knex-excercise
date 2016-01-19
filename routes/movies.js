daea016ab80e2269032c156b2daaec0413b65c83
= <tsmit291@icloud.com>
2016-01-19 16:10:37 -0700

    test_movie_reviews are all passing

--- routes/movies.js
+++ routes/movies.js
@@ -41,5 +41,32 @@ router.get('/', function (req, res, next){
   res.render('movies/index', {rows: allRows})
 });
 
+/*Test_Movie_Reviews Routes go here */
+router.get('/:movie_id/reviews', function (req, res, next){
+  res.render('movies/reviews/show');
+});
 
+router.get('/:movie_id/reviews/new', function (req, res, next){
+  res.render('movies/reviews/new');
+});
+
+router.post('/:movie_id/reviews', function (req, res, next){
+  res.redirect('/movies/new');
+});
+
+router.get('/:movie_id/reviews/:id', function (req, res, next){
+  res.render('movies/reviews/new');
+});
+
+router.get('/:movie_id/reviews/:id/edit', function (req, res, next){
+  res.render('movies/reviews/edit');
+});
+
+router.post('/:movie_id/reviews/:id', function (req, res, next){
+  res.redirect('/');
+});
+
+router.post('/:movie_id/reviews/:id/delete', function (req, res, next){
+  res.redirect('/')
+})
 module.exports = router;
--- test/test_movie_reviews_routes.js
+++ test/test_movie_reviews_routes.js
@@ -6,49 +6,49 @@ var server = require('../app');
 chai.use(chaiHttp);
 
 describe('Reviews CRUD Routes', function() {
-  xit('should GET /movies/:movie_id/reviews', function (done) {
+  it('should GET /movies/:movie_id/reviews', function (done) {
     chai.request(server).get('/movies/1/reviews')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should GET /movies/:movie_id/reviews/new', function (done) {
+  it('should GET /movies/:movie_id/reviews/new', function (done) {
     chai.request(server).get('/movies/2/reviews/new')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should POST /movies/:movie_id/reviews', function (done) {
+  it('should POST /movies/:movie_id/reviews', function (done) {
     chai.request(server).post('/movies/3/reviews')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should GET /movies/:movie_id/reviews/:id', function (done) {
+  it('should GET /movies/:movie_id/reviews/:id', function (done) {
     chai.request(server).get('/movies/2/reviews/1')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should GET /movies/:movie_id/reviews/:id/edit', function (done) {
+  it('should GET /movies/:movie_id/reviews/:id/edit', function (done) {
     chai.request(server).get('/movies/2/reviews/1/edit')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should POST to /movies/:movie_id/reviews/:id', function (done) {
+  it('should POST to /movies/:movie_id/reviews/:id', function (done) {
     chai.request(server).post('/movies/2/reviews/2')
     .end(function (err, res) {
       res.should.have.status(200);
       done();
     });
   });
-  xit('should POST to /movies/:movie_id/reviews/:id/delete', function (done) {
+  it('should POST to /movies/:movie_id/reviews/:id/delete', function (done) {
     chai.request(server).post('/movies/2/reviews/1/delete')
     .end(function (err, res) {
       res.should.have.status(200);
